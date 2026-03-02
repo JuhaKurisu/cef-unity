6ffefd8e586d8e0c354b5d06d92b4fbf41e737e2
Juha <juhasapps@gmail.com>
2026-03-03 02:01:40 +0900

    i

--- cef-unity-rust/build.rs
+++ cef-unity-rust/build.rs
@@ -5,6 +5,8 @@ fn main() {
         .csharp_namespace("CefUnity")
         .csharp_class_name("NativeMethods")
         .csharp_use_function_pointer(false)
-        .generate_csharp_file("dotnet/NativeMethods.g.cs")
+        .generate_csharp_file(
+            "../cef-unity-unityproject/Assets/CefUnity/Interop/NativeMethods.g.cs",
+        )
         .unwrap();
 }
